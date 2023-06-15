e38e9f380d48fba4c7c80f6703772ea565415a09
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-06-15 14:32:07 +0000

    create the model for comments from our diagram and migrage the changes

--- trade/models.py
+++ trade/models.py
@@ -5,8 +5,9 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Listing(models.Model): 
-    title = models.CharField(max_lenght=100)
-    slug = models.SlugField(max_lenght=200, unique=True)
+    
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
@@ -26,4 +27,19 @@ class Listing(models.Model):
         return self.likes.count()
 
 class Comment(models.Model):
+
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"comment {self.body} by {self.name}"
+
+
 
