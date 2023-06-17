81680f2897b494f788bbe384c4c4ca6b2580f866
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-06-17 14:46:45 +0000

    add the comment model to admin panel and enable the function to approve comments

--- trade/admin.py
+++ trade/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from .models import Listing
+from .models import Listing, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Listing)
 class ListingAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('description')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'listing', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
