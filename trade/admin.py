641800dca491e0b3e19bba2139124e619340f542
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-06-17 13:12:58 +0000

    add summernote to the project, and use it for the description field

--- trade/admin.py
+++ trade/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Listing
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Listing)
+
+@admin.register(Listing)
+class ListingAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
+
