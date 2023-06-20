d88c92932b6bd00c19929d2ac75d4f134a51b052
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-06-20 13:51:36 +0000

    add a class-based view for detail listings

--- trade/views.py
+++ trade/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Listing
 
 
@@ -8,3 +8,24 @@ class ListingList(generic.ListView):
     queryset = Listing.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 15
+
+
+class ListingDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Listing.objects.filter(status=1)
+        listing = get_object_or_404(queryset, slug=slug)
+        comments = listing.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if listing.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "listing_detail.html",
+            {
+                "listing": listing,
+                "comments": comments,
+                "liked": liked,
+            },
+        )
