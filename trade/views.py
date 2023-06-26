6fd1ae7a7066925299637a7be9dd587af607ada4
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-06-26 11:25:56 +0000

    introduce the possibility to review and approve comments from the admin panel

--- trade/views.py
+++ trade/views.py
@@ -27,6 +27,38 @@ class ListingDetail(View):
             {
                 "listing": listing,
                 "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Listing.objects.filter(status=1)
+        listing = get_object_or_404(queryset, slug=slug)
+        comments = listing.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if listing.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.listing = listing
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "listing_detail.html",
+            {
+                "listing": listing,
+                "comments": comments,
+                "commented": True,
                 "liked": liked,
                 "comment_form": CommentForm()
             },
