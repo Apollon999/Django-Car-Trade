dbf33e1cd831999cb7e6522a2288f7eec53b850d
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-06-27 18:56:32 +0000

    give the page some style with css

--- trade/admin.py
+++ trade/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Listing, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+# Register models in admin interface and customize views
 
 @admin.register(Listing)
 class ListingAdmin(SummernoteModelAdmin):
