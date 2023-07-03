00c80731e5fde0c1baedfe1ea4d47c1ba124999a
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-07-03 16:16:35 +0000

    start adding a listing form to the home page

--- trade/forms.py
+++ trade/forms.py
@@ -1,8 +1,11 @@
 from .models import Comment
 from django import forms
+from .models import Listing
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields =('body',)
+
+class ListingForm(forms.ModelForm):
