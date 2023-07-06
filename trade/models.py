2cf6d68b3e1fe11b18d1be1973aa75499e444ba3
Apollon Didrik Georgopoulos <diddec01@outlook.com>
2023-07-06 15:45:45 +0000

    add new models to the site

--- trade/models.py
+++ trade/models.py
@@ -10,6 +10,16 @@ class Listing(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="trade_listings")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    engine = models.CharField(max_length=50)
+    FUEL_CHOICES = [
+        ('gasoline', 'Gasoline'),
+        ('diesel', 'Diesel'),
+        ('electric', 'Electric'),
+        ('hybrid', 'Hybrid'),
+    ]
+    fuel = models.CharField(max_length=20, choices=FUEL_CHOICES)
+    year = models.IntegerField()
     updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
